f084e6ae712ec005a7a2583b0703e98fc12b9cde
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-06-07 12:07:18 +0200

    Add folding, format xml files on opening

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,2 +1,16 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+-- Folding settings, see
+-- https://www.jackfranklin.co.uk/blog/code-folding-in-vim-neovim/
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 5
+
+vim.api.nvim_create_autocmd( {"BufEnter", "BufWinEnter"}, {
+  pattern = {"*.xml", "*.xml.rels"},
+  callback = function()
+    local filename = vim.fn.expand('%:p')
+    vim.fn.system('xmllint --format --recover ' .. filename .. ' --output ' .. filename)
+  end
+})
